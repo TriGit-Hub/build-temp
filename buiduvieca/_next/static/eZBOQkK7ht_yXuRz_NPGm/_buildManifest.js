1202eefaba799193717f3bd1af2923c2955f5483
Trigit-hub <cjtr.1998@gmail.com>
2022-07-26 19:24:10 -0600

    static uvieca

---
+++ buiduvieca/_next/static/eZBOQkK7ht_yXuRz_NPGm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/483-705bbb07868bd7b0.js","static/css/c1f7eebb114481be.css","static/chunks/pages/index-20c56ff1120eed14.js"],"/_error":["static/chunks/pages/_error-98e801c646abf33c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
